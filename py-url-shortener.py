7b0f3341d6f120ca2f712eb855f1c7eb234eb3fb
awongCM <awong.cm@gmail.com>
2017-12-02 17:36:05 +1100

    refactored the script into a class

--- py-url-shortener.py
+++ py-url-shortener.py
@@ -7,71 +7,79 @@ import sys
 import base64
 import json
 
-# instantiate the local resider server
-redis_srv = redis.StrictRedis(host='localhost', port=6379, db=0)
-
-# global variables for redis implementation
-redis_shortened_url_key_fmt = 'shortened.url:%s'
-redis_global_urls_list = 'global:urls'
-redis_shortened_url_visitors_list_fmt = 'visitors:%s:url'
-redis_shortened_url_clicks_counter_fmt = 'clicks:%s:url'
-
-# Actionable methods
-def shorten_url(long_url):
-    # jumbled them
-    prefix_url = 'http://rllytny.url/'
-
-    url_str_arr = list(long_url)
-    random.shuffle(url_str_arr)
+class UrlShortenerService:
     
-    # get the last 10 items of the jumbled_url, assuming url is very longer than 20 chars
-    if len(url_str_arr) > 20:
-        shortened_url = url_str_arr[-10:]
-    else:
-        shortened_url = url_str_arr
+    # local variables for redis implementation
+    redis_srv = None
+    redis_shortened_url_key_fmt = 'shortened.url:%s'
+    redis_global_urls_list = 'global:urls'
+    redis_shortened_url_visitors_list_fmt = 'visitors:%s:url'
+    redis_shortened_url_clicks_counter_fmt = 'clicks:%s:url'
 
-    jumbled_url = ''.join(shortened_url)
+    # our prefix base_url for our url shortener service
+    base_url = 'http://rllytny.url/'
 
-    shortened_url = prefix_url + jumbled_url
+    def __init__(self):
+        "Initializes self"
+        self.redis_srv = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-    encoded_url = encode_base64(jumbled_url)
-    decoded_url = decode_base64(encoded_url)
+    # Actionable methods
+    def shorten_url(self, long_url):
+        # jumbled them up
+        url_str_arr = list(long_url)
+        random.shuffle(url_str_arr)
+        
+        # get the last 10 items of the jumbled_url, assuming url is very longer than 20 chars
+        if len(url_str_arr) > 20:
+            shortened_url = url_str_arr[-10:]
+        else:
+            shortened_url = url_str_arr
 
-    shortened_url_key = url_string_formatter(redis_shortened_url_key_fmt, shortened_url)
+        jumbled_url_suffix = ''.join(shortened_url)
 
-    redis_srv.set(shortened_url_key, long_url)
-    redis_srv.lpush(redis_global_urls_list, shortened_url)
+        shortened_url = self.base_url + jumbled_url_suffix
 
-    return shortened_url, long_url, encoded_url, decoded_url
+        # TODOS
+        encoded_url = encode_base64(jumbled_url_suffix)
+        decoded_url = decode_base64(encoded_url)
 
-def expand_url(shortened_url):
-    shortened_url_key = url_string_formatter(redis_shortened_url_key_fmt, shortened_url)
-    return redis_srv.get(shortened_url_key)
+        shortened_url_key = url_string_formatter(self.redis_shortened_url_key_fmt, shortened_url)
 
-def visit(shortened_url=None, ip_address=None, agent=None, referrer=None):
-    visitorAgent = {'ip_address': ip_address, 'agent':agent, 'referrer':referrer}
-    
-    url_visitors_list = url_string_formatter(redis_shortened_url_visitors_list_fmt, shortened_url)
-    redis_srv.lpush(url_visitors_list, json.dumps(visitorAgent))
+        self.redis_srv.set(shortened_url_key, long_url)
+        self.redis_srv.lpush(self.redis_global_urls_list, shortened_url)
 
-    url_clicks_counter = url_string_formatter(redis_shortened_url_clicks_counter_fmt, shortened_url)
-    return redis_srv.incr(url_clicks_counter)
+        return shortened_url, encoded_url, decoded_url
 
-# Retrieve counter properties from Redis
-def clicks(shortened_url = None):
-    url_clicks_counter = url_string_formatter(redis_shortened_url_clicks_counter_fmt, shortened_url)
-    return redis_srv.get(url_clicks_counter)
+    def expand_url(self, shortened_url):
+        shortened_url_key = url_string_formatter(self.redis_shortened_url_key_fmt, shortened_url)
+        return self.redis_srv.get(shortened_url_key)
 
-def recent_visitors(shortened_url):
-    visitorAgents = []
+    def visit(self, shortened_url=None, ip_address=None, agent=None, referrer=None):
+        visitorAgent = {'ip_address': ip_address, 'agent':agent, 'referrer':referrer}
+        
+        url_visitors_list = url_string_formatter(self.redis_shortened_url_visitors_list_fmt, shortened_url)
+        self.redis_srv.lpush(url_visitors_list, json.dumps(visitorAgent))
 
-    url_visitors_list = url_string_formatter(redis_shortened_url_visitors_list_fmt, shortened_url)
-    for v in redis_srv.lrange(url_visitors_list, 0, -1):
-        visitorAgents.append(json.loads(v))
-    return visitorAgents
+        url_clicks_counter = url_string_formatter(self.redis_shortened_url_clicks_counter_fmt, shortened_url)
+        return self.redis_srv.incr(url_clicks_counter)
 
-def short_urls():
-    return redis_srv.lrange(redis_global_urls_list, 0, 100)
+    # Retrieve counter properties from Redis
+    def clicks(self, shortened_url = None):
+        url_clicks_counter = url_string_formatter(self.redis_shortened_url_clicks_counter_fmt, shortened_url)
+        return self.redis_srv.get(url_clicks_counter)
+
+    def recent_visitors(self, shortened_url):
+        visitorAgents = []
+
+        url_visitors_list = url_string_formatter(self.redis_shortened_url_visitors_list_fmt, shortened_url)
+        for v in self.redis_srv.lrange(url_visitors_list, 0, -1):
+            visitorAgents.append(json.loads(v))
+        return visitorAgents
+
+    def short_urls(self):
+        return self.redis_srv.lrange(self.redis_global_urls_list, 0, 100)
+
+# End Class Methods
 
 #Utility methods
 def url_string_formatter(str_fmt, url):
@@ -86,44 +94,46 @@ def decode_base64(todecode):
     result = base64.b64decode(todecode)
     return result
 
-def readInputFile(text_file):
+def readInputFile(text_file, url_shortener_service):
     with open(text_file, 'r') as infile:
         for line in infile:
             # Ignore any comments in file
             if '#' not in line[0]:
-                shortened_url, raw_url, encoded_url, decoded_url = shorten_url(line)
-                expanded_url = expand_url(shortened_url)
+                shortened_url, encoded_url, decoded_url = url_shortener_service.shorten_url(line)
+                expanded_url = url_shortener_service.expand_url(shortened_url)
 
-                print("OriginalURL: {0}, NewURL: {1}, EncodedURL: {2}, DecodedURL: {3}".format(raw_url, shortened_url, encoded_url, decoded_url))
-                sys.stdout.write('.')
+                print("ShortenedURL: {0}; ExpandedURL: {1}".format(shortened_url, expanded_url))
+                print(".... EncodedURL: {0}; DecodedURL: {1}".format(encoded_url, decoded_url))
     return
 
-def visitors_visiting():
+def visitors_visiting(url_shortener_service):
 
     print('Visitors visiting...')
 
     for i in range(0, 5):    
-        for d in short_urls():
+        for d in url_shortener_service.short_urls():
             print('... %s' % d)
-            visit(d)
+            url_shortener_service.visit(d)
 
     print('Recent Visitors')
 
-    for d in short_urls():
+    for d in url_shortener_service.short_urls():
         print('... %s' % d)
-        visitor_agents = recent_visitors(d)
+        visitor_agents = url_shortener_service.recent_visitors(d)
         print('Total recent vistors for {0} are {1}'.format(d, len(visitor_agents)))
 
     return
 
 def main():
-    urls = sys.argv[1:]
+    
+    # inistantiate url_shorteer_service
+    url_shortener_service = UrlShortenerService()
 
     #read text input file
-    readInputFile('urls-to-read.txt')
+    readInputFile('urls-to-read.txt', url_shortener_service)
 
     #Web visitor activity being tracking...
-    visitors_visiting()
+    visitors_visiting(url_shortener_service)
 
 if __name__ == '__main__':
     main()
